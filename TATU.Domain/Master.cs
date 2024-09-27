44ec360365fd04e6391339cd917751cc808fba92
Влад Книгин <vlaknig@gmail.com>
2024-09-28 01:02:39 +0300

    создана новая модель мастер

--- TATU.Domain/Master.cs
+++ TATU.Domain/Master.cs
@@ -1,11 +1,17 @@
 ﻿namespace TATU.Domain
 {
-    public class Manager : Account
+    public class Master
     {
+        public Guid Id { get; set; }
         public string Name { get; set; } = default!;
         public string Surname { get; set; } = default!;
         public string? Patronymic { get; set; } = default!;
         public double WorkExperience { get; set; }
+        public string? Description { get; set; } = default!;
         public IList<Order>? Orders { get; set; }
+        public bool IsDismissed { get; set; } = false;
+        public string? PhotoAvatarLink { get; set; }
+        public IEnumerable<string>? PhotosWorksLink { get; set; }
+
     }
 }
