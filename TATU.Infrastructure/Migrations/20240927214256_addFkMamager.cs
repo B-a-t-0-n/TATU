be2d286922d9856e9b08a38547bf23bb50623785
Влад Книгин <vlaknig@gmail.com>
2024-09-28 00:48:01 +0300

    корректировка бд

---
+++ TATU.Infrastructure/Migrations/20240927214256_addFkMamager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TATU.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addFkMamager : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ManagerId",
+                table: "Orders",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ManagerId",
+                table: "Orders",
+                column: "ManagerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Managers_ManagerId",
+                table: "Orders",
+                column: "ManagerId",
+                principalTable: "Managers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Managers_ManagerId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_ManagerId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ManagerId",
+                table: "Orders");
+        }
+    }
+}
