913077d16191974c6150a92c53174a72073f9101
Влад Книгин <vlaknig@gmail.com>
2024-09-30 23:57:18 +0300

    исправлены ошибки добавлена возможность обновлять информацио о услуге

--- TATU.Application/Services/Delete/DeleteServicesHandler.cs
+++ TATU.Application/Services/Delete/DeleteServicesHandler.cs
@@ -12,7 +12,7 @@
         public async Task<Guid> Handle(DeleteServicesCommand command)
         {
             var services = await _servicrsRepository.GetById(command.Id)
-                ?? throw new ArgumentNullException("Master is null");
+                ?? throw new ArgumentNullException("services is null");
 
             await _servicrsRepository.Delete(services);
 
---
+++ TATU.Application/Services/Update/UpdateServicesHandler.cs
@@ -0,0 +1,32 @@
+﻿using TATU.Application.Masters.Update;
+using TATU.Application.Masters;
+
+namespace TATU.Application.Services.Update
+{
+    public class UpdateServicesHandler
+    {
+        private readonly IServicesRepository _servicrsRepository;
+
+        public UpdateServicesHandler(IServicesRepository servicrsRepository)
+        {
+            _servicrsRepository = servicrsRepository;
+        }
+
+        public async Task<Guid> Handle(UpdateServicesCommand command)
+        {
+            var services = await _servicrsRepository.GetById(command.ServicesId)
+                ?? throw new ArgumentNullException("services is null");
+
+            var title = command.Services.Title;
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentNullException("Services.title is null or white space");
+
+            services.Title = title;
+            services.Description = command.Services.Description;
+
+            await _servicrsRepository.Save(services);
+
+            return services.Id;
+        }
+    }
+}
