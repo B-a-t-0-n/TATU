7e0363658a9fedb4795c1f82f8ced76ba42529f3
Влад Книгин <vlaknig@gmail.com>
2024-09-30 22:26:23 +0300

    реализован интерфейс IPasswordHasher и добавлен класс inject

---
+++ TATU.Infrastructure/Inject.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TATU.Application.Accounts;
+using TATU.Application.Masters;
+using TATU.Application.Providers;
+using TATU.Application.Services;
+using TATU.Infrastructure.Providers;
+using TATU.Infrastructure.Repository;
+
+namespace TATU.Infrastructure
+{
+    public static class Inject
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddScoped<ApplicationDbContext>();
+            services.AddScoped<IAccountRepository, AccountRepository>();
+            services.AddScoped<IMasterRepository, MasterRepository>();
+            services.AddScoped<IServicesRepository, ServicesRepository>();
+
+            services.AddTransient<IPasswordHasher, PasswordHasher>();
+
+            return services;
+        }
+    }
+}
---
+++ TATU.Infrastructure/Providers/PasswordHasher.cs
@@ -0,0 +1,10 @@
+﻿using TATU.Application.Providers;
+
+namespace TATU.Infrastructure.Providers
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Generate(string password) => BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+        public bool Verefy(string password, string heshedPassword) => BCrypt.Net.BCrypt.EnhancedVerify(password, heshedPassword);
+    }
+}
