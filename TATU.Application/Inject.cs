fd0297300b1233a5cb36da8280330deb912d53bc
Влад Книгин <vlaknig@gmail.com>
2024-09-30 23:31:03 +0300

    реализованна возможность обновлять информацию о мастере

--- TATU.Application/Inject.cs
+++ TATU.Application/Inject.cs
@@ -2,6 +2,7 @@
 using TATU.Application.Accounts.AddClient;
 using TATU.Application.Accounts.Create;
 using TATU.Application.Masters.Create;
+using TATU.Application.Masters.Update;
 using TATU.Application.Services.Create;
 
 namespace TATU.Application
@@ -13,6 +14,7 @@ namespace TATU.Application
             services.AddScoped<CreateAccountManagerHandler>();
             services.AddScoped<AddOrderHandler>();
             services.AddScoped<CreateMasterHandler>();
+            services.AddScoped<UpdateMasterHandler>();
             services.AddScoped<CreateServicesHandler>();
 
             return services;
---
+++ TATU.Application/Masters/Update/UpdateMasterHandler.cs
@@ -0,0 +1,42 @@
+﻿using TATU.Application.Masters.Create;
+using TATU.Domain;
+
+namespace TATU.Application.Masters.Update
+{
+    public class UpdateMasterHandler
+    {
+        private readonly IMasterRepository _masterRepository;
+
+        public UpdateMasterHandler(IMasterRepository masterRepository)
+        {
+            _masterRepository = masterRepository;
+        }
+
+        public async Task<Guid> Handle(UpdateMasterCommand command)
+        {
+            var master = await _masterRepository.GetById(command.MasterId)
+                ?? throw new ArgumentNullException("Master is null");
+
+            var name = command.Master.Name;
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException("Master.name is null or white space");
+
+            var surname = command.Master.Surname;
+            if (string.IsNullOrWhiteSpace(surname))
+                throw new ArgumentNullException("Master.surname is null or white space");
+
+            master.Name = name;
+            master.Surname = surname;
+            master.Patronymic = command.Master.Patronymic;
+            master.IsDismissed = command.Master.IsDismissed;
+            master.Description = command.Master.Description;
+            master.PhotoAvatarLink = command.Master.PhotoAvatarLink;
+            master.PhotosWorksLink = command.Master.PhotosWorksLink;
+            master.WorkExperience = command.Master.WorkExperience;
+
+            await _masterRepository.Save(master);
+
+            return master.Id;
+        }
+    }
+}
