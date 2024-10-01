8a920e188079cb38b00082d5fb9d95a29dd62615
Влад Книгин <vlaknig@gmail.com>
2024-10-01 22:48:03 +0300

    добавлена возморность получения всех аккаунтов

---
+++ TATU.Application/Accounts/GetAll/GetAllAccountHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace TATU.Application.Accounts.GetAll
+{
+    public class GetAllAccountHandler
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public GetAllAccountHandler(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public async Task<List<Guid>> Handle()
+        {
+            var accounts = await _accountRepository.GetAll();
+
+            return accounts.Select(a => a.Id).ToList();
+        }
+    }
+}
