a61c646cf87e578b8e7ffd92c0b0228107037663
Влад Книгин <vlaknig@gmail.com>
2024-10-01 23:08:20 +0300

    добавленна возможность получения всех мастеров

---
+++ TATU.Application/Masters/GetAll/GetAllMasterHandler.cs
@@ -0,0 +1,21 @@
+﻿using TATU.Domain;
+
+namespace TATU.Application.Masters.GetAll
+{
+    public class GetAllMasterHandler
+    {
+        private readonly IMasterRepository _masterRepository;
+
+        public GetAllMasterHandler(IMasterRepository masterRepository)
+        {
+            _masterRepository = masterRepository;
+        }
+
+        public async Task<List<Master>> Handle()
+        {
+            var masters = await _masterRepository.GetAll();
+
+            return masters;
+        }
+    }
+}
