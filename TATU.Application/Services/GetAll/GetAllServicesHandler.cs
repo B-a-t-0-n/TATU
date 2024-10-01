a32b806991af555ef3f30042c248b8e4eff86c6e
Влад Книгин <vlaknig@gmail.com>
2024-10-01 23:15:50 +0300

    добавлена возможность получения всех услуг

---
+++ TATU.Application/Services/GetAll/GetAllServicesHandler.cs
@@ -0,0 +1,22 @@
+﻿using TATU.Application.Masters;
+using TATU.Domain;
+
+namespace TATU.Application.Services.GetAll
+{
+    public class GetAllServicesHandler
+    {
+        private readonly IServicesRepository _servicesRepository;
+
+        public GetAllServicesHandler(IServicesRepository servicesRepository)
+        {
+            _servicesRepository = servicesRepository;
+        }
+
+        public async Task<List<Domain.Services>> Handle()
+        {
+            var services = await _servicesRepository.GetAll();
+
+            return services;
+        }
+    }
+}
